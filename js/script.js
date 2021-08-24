8c7116ffef25e729e0682e9ec489c7275e6859b6
Ole994 <69842859+Ole994@users.noreply.github.com>
2021-08-24 17:16:44 +0200

    123

---
+++ js/script.js
@@ -0,0 +1,14 @@
+const list = document.querySelector (".js-stuff");
+
+const arrayList = [
+    //Starting on a object
+    {
+        img: "http://stoltzen.no/bilder/bestill.php?bf=93976_2019_6.jpg&bn=Ole%20J%20Korvald&bt=14.42&ba=2019",
+    },
+ 
+];
+
+for (i of arrayList){
+    
+}
+
